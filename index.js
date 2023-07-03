58f02e410e9a768953f2e8fadbda57110bb27b55
kayzweller <kayzweller@gmail.com>
2023-07-03 20:45:53 +0700

    init --puppeteer

---
+++ index.js
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+
+  // Create a browser instance
+  const browser = await puppeteer.launch({
+    headless: 'new',
+    // `headless: true` (default) enables old Headless;
+    // `headless: 'new'` enables new Headless;
+    // `headless: false` enables “headful” mode.
+  });
+
+  // Create a new page
+  const page = await browser.newPage();
+
+  //Get HTML content from HTML file
+  const html = fs.readFileSync('sample.html', 'utf-8');
+  await page.setContent(html, { waitUntil: 'domcontentloaded' });
+
+  // To reflect CSS used for screens instead of print
+  await page.emulateMediaType('screen');
+
+  // Download the PDF
+  const pdf = await page.pdf({
+    path: 'result.pdf',
+    margin: { top: '100px', right: '50px', bottom: '100px', left: '50px' },
+    printBackground: true,
+    format: 'A4',
+  });
+
+  // Close the browser instance
+  await browser.close();
+})();
